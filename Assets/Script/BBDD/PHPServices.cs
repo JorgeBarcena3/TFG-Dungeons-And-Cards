eb492de28ba1d125cb76944907f4823df88a8854
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2020-01-03 13:23:28 +0100

    wwwForm

---
+++ Assets/Script/BBDD/PHPServices.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Clase que contiene los servicios php
+/// </summary>
+public class PHPServices : MonoBehaviour
+{
+    /// <summary>
+    /// URL de conexion con el servidor
+    /// </summary>
+    public static string url = "http://localhost/phpmyadmin/dungeon/";
+
+    /// <summary>
+    /// Obtenemos todos los scores
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerator GetAllScores()
+    {
+        string API_URL = url + "getAllScores.php";
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+
+        formData.Add(new MultipartFormDataSection("serverName=" + url + "&username=root$password=''"));
+
+        UnityWebRequest www = UnityWebRequest.Post(API_URL, formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            string results = www.downloadHandler.text;
+            yield return results;
+        }
+
+    }
+
+    /// <summary>
+    /// Almacenamos los scores en la base de datos
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="score"></param>
+    /// <returns></returns>
+    public IEnumerator setScore(string name, string score)
+    {
+        string API_URL = url + "setScore.php";
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+
+        formData.Add(new MultipartFormDataSection("serverName=" + url + "&username=root$password=&score="+ score + "&name=" + name));
+
+        UnityWebRequest www = UnityWebRequest.Post(API_URL, formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            yield return www.error;
+        }
+        else
+        {
+            string results = www.downloadHandler.text;
+            yield return results;
+        }
+
+    }
+
+
+}
---
+++ Assets/Script/BBDD/dto/scoreDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Modelo de datos de la tabla score
+/// </summary>
+public class scoreDto
+{
+    /// <summary>
+    /// Nombre
+    /// </summary>
+    string Name { get; set; }
+
+    /// <summary>
+    /// Puntuacion
+    /// </summary>
+    string Score { get; set; }
+
+    /// <summary>
+    /// Fecha
+    /// </summary>
+    DateTime Date { get; set; }
+}
+
