471611331c993c0c8409527e15214843655633e1
miguelangelgil <miguel.gilmartin12@gmail.com>
2020-02-03 18:58:58 +0100

    water

---
+++ Assets/Script/Tools/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    private Transform tr;
+    public bool coord2D;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coord2D)
+            tr.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, distance);
+        else 
+        {
+            tr.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z + distance);
+        }
+    }
+}
