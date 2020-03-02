1b2888f8bbf3300a0a9682259375a00b9a03c2cc
miguelangelgil <miguel.gilmartin12@gmail.com>
2020-03-02 20:02:17 +0100

    merge

--- Assets/Script/Cards/Card/CSVReader.cs
+++ Assets/Script/Cards/Card/CSVReader.cs
@@ -9,20 +9,34 @@
  
 	Drag onto a gameobject for a demo of CSV parsing.
 */
-
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
 using System.Linq;
 
 public class CSVReader : MonoBehaviour
 {
+    private List<InfoCard> cardsInfo;
     public TextAsset csvFile;
     public void Start()
     {
         string[,] grid = SplitCsvGrid(csvFile.text);
         Debug.Log("size = " + (1 + grid.GetUpperBound(0)) + "," + (1 + grid.GetUpperBound(1)));
 
-        DebugOutputGrid(grid);
+        for (int i = 1; i < grid.GetUpperBound(1); i++) 
+        {
+            if (grid[0, i] != null) 
+            {
+                string info = grid[0, i];
+                string[] parametres = info.Split(';');
+                cardsInfo.Add(new InfoCard((ATTACKTYPE)int.Parse(parametres[0]), int.Parse(parametres[1]), parametres[2], 
+                    parametres[3], int.Parse(parametres[4]), int.Parse(parametres[5]), int.Parse(parametres[6])));
+            }
+            
+        }
+
+        //DebugOutputGrid(grid);
     }
 
     // outputs the content of a 2D array, useful for checking the importer
@@ -49,7 +63,7 @@ public class CSVReader : MonoBehaviour
 
         // finds the max width of row
         int width = 0;
-        for (int i = 0; i < lines.Length; i++)
+        for (int i = 1; i < lines.Length; i++)
         {
             string[] row = SplitCsvLine(lines[i]);
             width = Mathf.Max(width, row.Length);
@@ -57,7 +71,7 @@ public class CSVReader : MonoBehaviour
 
         // creates new 2D string grid to output to
         string[,] outputGrid = new string[width + 1, lines.Length + 1];
-        for (int y = 0; y < lines.Length; y++)
+        for (int y = 1; y < lines.Length; y++)
         {
             string[] row = SplitCsvLine(lines[y]);
             for (int x = 0; x < row.Length; x++)
@@ -80,5 +94,9 @@ public class CSVReader : MonoBehaviour
         @"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)",
         System.Text.RegularExpressions.RegexOptions.ExplicitCapture)
                 select m.Groups[1].Value).ToArray();
+    }
+    public List<InfoCard> getCardsInfo() 
+    {
+        return cardsInfo;
     }
 }
