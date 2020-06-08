4d103cfba6e66a41280e768c8d74342555108b2e
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2020-06-08 13:18:21 +0200

    Error

--- Assets/Script/Cards/Deck/Menu/DeckCollection.cs
+++ Assets/Script/Cards/Deck/Menu/DeckCollection.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using Newtonsoft.Json;
-using UnityEditor.Rendering.LWRP;
 
 public class DeckCollection
 {
